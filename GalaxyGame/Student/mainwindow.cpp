079c4a803888512b56cd00e9de0f3e80f128ac2e
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-08 12:25:05 +0200

    add interface for updating UI

--- GalaxyGame/Student/mainwindow.cpp
+++ GalaxyGame/Student/mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "constants.h"
-
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,9 +13,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->gameNameLabel->setText(Constants::gameName);
     ui->authorLabel->setText(Constants::author);
     ui->versionLabel->setText(Constants::version);
+    galaxy_scene = new QGraphicsScene(QRect(0, 0, 1000, 1000));
+    ui->galaxyView->setScene(galaxy_scene);
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::addShipToGalaxyScene(int x, int y)
+{
+
+}
+
+void MainWindow::addStarSystemToGalaxyScene(int x, int y)
+{
+
 }
--- GalaxyGame/Student/mainwindow.h
+++ GalaxyGame/Student/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QGraphicsScene>
 namespace Ui {
 class MainWindow;
 }
@@ -14,9 +14,11 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+    void addShipToGalaxyScene(int x, int y);
+    void addStarSystemToGalaxyScene(int x, int y);
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene* galaxy_scene;
 };
 
 #endif // MAINWINDOW_H
