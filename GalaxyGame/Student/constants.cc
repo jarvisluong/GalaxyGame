1487a464adabfc1ad187a8887099ec00e61b81e4
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-08 14:16:35 +0200

    add constants source file

---
+++ GalaxyGame/Student/constants.cc
@@ -0,0 +1,12 @@
+#include "constants.hh"
+
+Constants::Constants()
+{
+
+}
+
+const QString Constants::gameName = "HSALAXY NAME";
+
+const QString Constants::author = "Author: Hai & Sang";
+
+const QString Constants::version = "v1.0";
