34e9cc4810b66bc13fd43f6c52411571a410eb0b
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-08 14:17:01 +0200

    connect signals from Galaxy to slot in MainWindow

--- GalaxyGame/Student/main.cc
+++ GalaxyGame/Student/main.cc
@@ -3,7 +3,7 @@
 #include "igamerunner.hh"
 #include "utility.hh"
 #include "eventhandler.hh"
-#include "mainwindow.h"
+#include "mainwindow.hh"
 #include <QApplication>
 #include <memory>
 #include <time.h>
@@ -15,9 +15,15 @@ int main(int argc, char *argv[])
 
     std::shared_ptr<Common::IEventHandler> handler = std::make_shared<Student::EventHandler>();
     std::shared_ptr<Student::Galaxy> galaxy = std::make_shared<Student::Galaxy>();
-    std::shared_ptr<Common::IGameRunner> gameRunner = Common::getGameRunner(galaxy, handler);
-    Common::utilityInit(time(NULL));
 
+    Student::Galaxy* myGalaxy = galaxy.get();
+    MainWindow::connect(myGalaxy, SIGNAL(shipAddedAtCoordinates(int, int)), &w, SLOT(addShipToGalaxyScene(int,int)));
+    MainWindow::connect(myGalaxy, SIGNAL(starSystemAddedAtCoordinates(int,int)), &w, SLOT(addStarSystemToGalaxyScene(int,int)));
+
+    std::shared_ptr<Common::IGameRunner> gameRunner = Common::getGameRunner(galaxy, handler);
+
+    Common::utilityInit(time(NULL));
+    gameRunner->spawnShips(5);
     w.show();
 
     return a.exec();
