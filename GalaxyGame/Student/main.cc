3ed6d16f7e9d7a7748e112c1c6565aa97f6237dd
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-16 11:33:33 +0200

    setGalaxy for MainWindow and add TODO

--- GalaxyGame/Student/main.cc
+++ GalaxyGame/Student/main.cc
@@ -10,6 +10,8 @@
 #include <time.h>
 #include <QDebug>
 
+// TODO: ask TA why Student::Galaxy cannot be a property in MainWindow class
+// Resolve now by static_cast- maybe not a good idea.
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -18,10 +20,13 @@ int main(int argc, char *argv[])
     std::shared_ptr<Student::Galaxy> galaxy = std::make_shared<Student::Galaxy>();
 
     Student::Galaxy* myGalaxy = galaxy.get();
+    Common::IGalaxy* igalaxy = static_cast<Common::IGalaxy*>(myGalaxy);
     MainWindow w;
+    w.setGalaxy(igalaxy);
     MainWindow::connect(myGalaxy, SIGNAL(starSystemAddedAtCoordinates(std::shared_ptr<Common::StarSystem>)), &w, SLOT(addStarSystemToGalaxyScene(std::shared_ptr<Common::StarSystem>)));
     std::shared_ptr<Common::IGameRunner> gameRunner = Common::getGameRunner(galaxy, handler);
     Common::utilityInit(time(NULL));
+
 
     w.setEventHandler(handler);
     w.setGameRunner(gameRunner);
@@ -30,6 +35,7 @@ int main(int argc, char *argv[])
         w.getGameRunner()->spawnShips(configWindow.getNumberOfShips());
         w.show();
     }
+//    Student::EventHandler* student_handler = dynamic_cast<Student::EventHandler*>(handler.get());
 
 
     return a.exec();
