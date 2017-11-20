5fd78609852d6bb24a31a92805f91035b27f225b
Luong Dang Hai <hailuongdang1998@gmail.com>
2017-11-20 14:02:54 +0200

    document playership class

--- GalaxyGame/Student/playership.h
+++ GalaxyGame/Student/playership.h
@@ -6,22 +6,85 @@
 #include <QGraphicsPixmapItem>
 
 namespace Student {
+/*!
+ * \brief The PlayerShip class
+ */
 class PlayerShip
 {
 public:
+    /*!
+     * \brief PlayerShip constructor
+     * \param initialLocation initial location for the ship to be put in the system
+     * \param initialHealth initial health for the ship
+     */
     PlayerShip(Common::Point initialLocation, unsigned int initialHealth = 50);
+
+    /*!
+     * \brief goToLocation this method makes the ship to change location in the system
+     * \param nextLocation new location to move to
+     */
     void goToLocation(Common::Point nextLocation);
+
+    /*!
+     * \brief decreaseHealth decrease health of the ship
+     * \param amount amount of health to decrease
+     */
     void decreaseHealth(unsigned int amount);
+
+    /*!
+     * \brief increaseHealth increase health of the ship
+     * \param amount amount of health to increase
+     */
     void increaseHealth(unsigned int amount);
+
+    /*!
+     * \brief getHealth get the current health of the ship
+     * \return the health of the ship
+     */
     unsigned int getHealth();
+
+    /*!
+     * \brief getLocation get the current location of the ship
+     * \return the location of the ship
+     */
     Common::Point getLocation();    
+
+    /*!
+     * \brief set_ui_item set the reference to the UI display item of the ship
+     * \param item the pointer to the PixMap item of the ship
+     */
     void set_ui_item(QGraphicsPixmapItem *item);
+
+    /*!
+     * \brief get_ui_item get the reference to the UI display item of the ship
+     * \return the pointer to the PixMap item of the ship
+     */
     QGraphicsPixmapItem *get_ui_item();
 private:
+    /*!
+     * \brief invariant
+     * \pre health always between 0 and maxHealth
+     */
     void invariant();
+
+    /*!
+     * \brief ui_item the pointer to the UI display item of the ship
+     */
     QGraphicsPixmapItem* ui_item;
+
+    /*!
+     * \brief maxHealth maxHealth allow for the ship
+     */
     const unsigned int maxHealth = 50;
+
+    /*!
+     * \brief _location current location of the ship
+     */
     Common::Point _location;
+
+    /*!
+     * \brief _health current health of the ship
+     */
     unsigned int _health;
 };
 }
