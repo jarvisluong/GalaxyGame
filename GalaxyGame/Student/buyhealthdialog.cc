062c601b2673e41f963130a5991f51c4ab19baf1
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-21 02:05:03 +0200

    detail implementation for setCreditsForText

--- GalaxyGame/Student/buyhealthdialog.cc
+++ GalaxyGame/Student/buyhealthdialog.cc
@@ -1,13 +1,6 @@
 #include "buyhealthdialog.hh"
 #include "ui_buyhealthdialog.h"
 
-//BuyHealthDialog::BuyHealthDialog(QWidget *parent) :
-//    QDialog(parent),
-//    ui(new Ui::BuyHealthDialog)
-//{
-//    ui->setupUi(this);
-
-//}
 
 BuyHealthDialog::BuyHealthDialog(unsigned credits_, QWidget *parent):
     QDialog(parent),
@@ -28,13 +21,24 @@ int BuyHealthDialog::getNumberOfHealthToBuy()
     return ui->healthspinBox->value();
 }
 
-void BuyHealthDialog::setCreditsForText(int credits_)
+void BuyHealthDialog::setCreditsForText(int credits_, int health_)
 {
     ui->credits_price_label->setText("You have " + QString::number(credits_) + " credits. Price: 5 credits for 1 health.");
-    ui->healthspinBox->setMaximum(credits_ / 5);
+    ui->healthspinBox->setMaximum(std::min(credits_ / 5, Constants::playerMaxHealth - health_));
+    if(credits_ / 5 > Constants::playerMaxHealth - health_) {
+//        ui->notificationLabel->setStyleSheet("QLabel {color: red}");
+        QFont font;
+        font.setBold(true);
+        ui->notificationLabel->setFont(font);
+        ui->notificationLabel->setText("*Maximum health is " + QString::number(Constants::playerMaxHealth) + ". Buy "
+                                       + QString::number(Constants::playerMaxHealth - health_) + " is enough.");
+        ui->notificationLabel->show();
+    }
 }
 
 void BuyHealthDialog::on_buyButton_clicked()
 {
+    ui->notificationLabel->hide();
     emit on_buy_btn_clicked();
 }
+
