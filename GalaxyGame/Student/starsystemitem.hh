cff72422bf2323f79f393dfab03d996283f91c17
Nguyen The Sang <ntsang1702@gmail.com>
2017-11-22 00:31:21 +0200

    Add documentation

--- GalaxyGame/Student/starsystemitem.hh
+++ GalaxyGame/Student/starsystemitem.hh
@@ -2,11 +2,21 @@
 #define STARSYSTEMITEM_HH
 #include "customitem.hh"
 #include <QPixmap>
+/*!
+ * \brief The StarSystemItem class represent a star system image in the galaxy
+ */
 class StarSystemItem: public CustomItem
 {
 public:
+    /*!
+     * \brief StarSystemItem Constructor for the class, used the constructor from parent class.
+     */
     StarSystemItem(QPixmap pixmap_, Common::Point location = Common::Point(0,0));
 private:
+    /*!
+     * \brief mousePressEvent Event when click on a star system image.
+     * \param event
+     */
     void mousePressEvent(QGraphicsSceneMouseEvent* event);
 };
 
