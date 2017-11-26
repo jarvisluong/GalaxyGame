6c0a1850b461c0eab58afcc3c740f2c0f84ece81
Luong Dang Hai <hailuongdang1998@gmail.com>
2017-11-26 10:34:54 +0200

    add more documentation

--- GalaxyGame/Student/buyhealthdialog.hh
+++ GalaxyGame/Student/buyhealthdialog.hh
@@ -8,14 +8,34 @@ namespace Ui {
 class BuyHealthDialog;
 }
 
+/*!
+ * \brief The BuyHealthDialog class display the dialog for user to buy more health
+ */
 class BuyHealthDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /*!
+     * \brief BuyHealthDialog constructor of this dialog
+     * \param credits_ number of credits that player currently has
+     * \param parent QWidget parent
+     */
     explicit BuyHealthDialog(unsigned credits_, QWidget *parent = 0);
+
     ~BuyHealthDialog();
+
+    /*!
+     * \brief getNumberOfHealthToBuy get the number of health that player inputs to buy
+     * \return
+     */
     int getNumberOfHealthToBuy();
+
+    /*!
+     * \brief setCreditsForText hints the player about the amount of health they can buy
+     * \param credits_ the amount of credits that player currently has
+     * \param health_ the amount of health that player currently has
+     */
     void setCreditsForText(int credits_, int health_);
 signals:
     void on_buy_btn_clicked();
